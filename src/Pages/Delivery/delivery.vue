66fed1c6bd4f9d23bef6dc8feed4561e1d229962
Takitok2011 <bekzodeoke@gmail.com>
2025-02-22 23:08:43 +0300

    delivery maybe finished

---
+++ src/Pages/Delivery/delivery.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="page">
+    <div class="page-top d-flex a-center j-between">
+      <h3>Yetkazuvchilar</h3>
+      <button class="create-button" @click="openModal = true">
+        Yetkazuvchi yaratish
+      </button>
+    </div>
+    <div class="scroll page-bottom p-24">
+      <table>
+        <thead>
+          <tr>
+            <th>№</th>
+            <th>username</th>
+            <th>phone</th>
+            <th>password</th>
+            <th>price</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(data, index) in allDelivery" :key="index">
+            <td>{{ index + 1 }}</td>
+            <td>{{ data?.username ? data?.username : "" }}</td>
+            <td>{{ data?.phone ? data?.phone : "" }}</td>
+            <td>---------</td>
+            <td>{{ data?.price ? data?.price : "" }}</td>
+            <td class="d-flex a-center j-end gap12">
+              <Icons name="setting" title="sozlama" class="icon info setting" />
+              <Icons
+                name="deleted"
+                title="o'chirish"
+                class="icon danger"
+                @click="deleteDelivery(data?._id)"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <DeliveryModelVue v-if="openModal" @close="handleClose" />
+</template>
+
+<script>
+import api from "@/Utils/axios";
+import Icons from "@/components/Template/Icons.vue";
+import DeliveryModelVue from "./deliveryModel.vue";
+export default {
+  components: {
+    Icons,
+    DeliveryModelVue,
+  },
+  data() {
+    return {
+      allDelivery: [],
+      openModal: false,
+    };
+  },
+  methods: {
+    handleClose() {
+      this.openModal = false;
+      this.getDeliveries();
+    },
+    getDeliveries() {
+      const token = localStorage.getItem("user")
+        ? JSON.parse(localStorage.getItem("user"))?.accessToken
+        : "";
+
+      api
+        .get("/api/deliveries", {
+          headers: {
+            authorization: token,
+          },
+        })
+        .then(({ data, status }) => {
+          if (status === 200) {
+            this.allDelivery = data?.deliveries;
+          }
+        });
+    },
+    deleteDelivery(id) {
+      const token = localStorage.getItem("user")
+        ? JSON.parse(localStorage.getItem("user"))?.accessToken
+        : "";
+        api.delete("/api/delivery/" + id,{
+            headers:{
+                "authorization":token
+            }
+        }).then(({status})=>{
+            if(status === 200){
+                this.getDeliveries()
+            }
+        })
+    },
+  },
+  mounted() {
+    this.getDeliveries();
+  },
+};
+</script>
+
+<style>
+</style>
