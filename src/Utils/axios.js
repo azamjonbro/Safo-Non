451c3273694074c3e93c75d6685c0094efabf822
azamjonbro <azamjondasturchi11@gmail.com>
2025-03-01 23:31:15 +0500

    change ahios baseURL

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://api.alphae.uz',
+  baseURL: 'https://api.alphae.uz',
   // baseURL:"http://localhost:3500",
   withCredentials: true,
   timeout: 40000,
