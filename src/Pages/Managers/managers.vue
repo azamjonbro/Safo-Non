75bbd0243c50470a7467a8a71a6f2711b3c76f38
azamjonbro <azamjondasturchi11@gmail.com>
2025-03-05 03:52:31 +0500

    added page mainager

--- src/Pages/Managers/managers.vue
+++ src/Pages/Managers/managers.vue
@@ -4,7 +4,19 @@
             <h3>Boshqaruvchilar</h3>
             <button class="create-button">Manager yaratish</button>
         </div>
-            <div class="page-bottom"></div>
+        <div class="page-bottom scroll p-24">
+            <div class="table">
+                <div class="table-header">
+                    <div class="row">
+                        <div class="cell">№</div>
+                        <div class="cell">Ismi</div>
+                        <div class="cell">Date</div>
+                        <div class="cell"></div>
+                        <div class="cell">item</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
