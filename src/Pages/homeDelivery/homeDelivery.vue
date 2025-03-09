81ae6c1a39d0113dea0db0b5167593ca736a84f3
Takitok2011 <bekzodeoke@gmail.com>
2025-03-10 02:35:05 +0300

    I will sleep after 5 minutes

---
+++ src/Pages/homeDelivery/homeDelivery.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page">
+    <div class="page-top d-flex j-between a-center">
+      <h3 style="padding: 12px 0px">Dashboard</h3>
+    </div>
+    <div class="page-bottom scroll">
+     
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+<style>
+
+</style>
