06b99dc9fb8ff53f5a1e10d2f07f3503cf9c45a5
Bekzod <bekzodeoke@gmail.com>
2025-04-06 03:45:44 +0500

    changed code

--- src/Pages/Magazine/magazine.vue
+++ src/Pages/Magazine/magazine.vue
@@ -75,6 +75,7 @@
                   <div class="cell">Yaratilgan</div>
                   <div class="cell">Yetkazuvchi</div>
                   <div class="cell">Soni</div>
+                  <div class="cell">Olingan pul</div>
                   <div class="cell">Umumiy pul</div>
                   <div class="cell">Qarzdorlik</div>
                   <div class="cell"></div>
@@ -88,6 +89,7 @@
                   </div>
                   <div class="cell">{{ item.deliveryId?.username }}</div>
                   <div class="cell">{{ formatPrice(item.quantity) }}</div>
+                  <div class="cell">{{ formatPrice(item.money) }}</div>
                   <div class="cell">{{ formatPrice(item.totalPrice) }}</div>
                   <div class="cell">{{ formatPrice(item.pending) }}</div>
                   <div class="cell d-flex j-end">
